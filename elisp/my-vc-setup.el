7aa08e2066e4be2cffa6d1cb9a759a20fd238c97
Max Mikhanosha <max@openchat.com>
2013-12-01 07:34:37 -0500

    Fix for new egg

--- elisp/my-vc-setup.el
+++ elisp/my-vc-setup.el
@@ -113,7 +113,7 @@ for the diff switches"
 (defun my-egg-status (&optional arg)
   "Call `egg-status' with T parameter so it switches to status buffer"
   (interactive)
-  (egg-status t)
+  (egg-status t nil)
   ;; (egg-buffer-hide-section-type :diff)
   )
 
