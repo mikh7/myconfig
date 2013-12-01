5f026d3f5fc92432a79d60e65db148c1b53ff5e9
Max Mikhanosha <max@openchat.com>
2013-12-01 07:33:04 -0500

    Get rid of mydump, its now its own project

--- dotfiles/swank.lisp
+++ dotfiles/swank.lisp
@@ -113,7 +113,7 @@
     (values path type lines)))
 
 
-(defparameter *mm/log-config* '(:sane2 :thread -12 :pretty :nopackage :info))
+(defparameter *mm/log-config* '(:sane2 :thread -12 :ndc :pretty :nopackage :info))
 
 (defun mm/reset-logging ()
   (log4cl:clear-logging-configuration)
@@ -143,92 +143,5 @@
        :completion-function #'complete
        :communication-style style)
       (format nil "Started dumping lisp to ~A..." image-file))))
-
-#+sbcl
-(defun mydump (filename &key restart-function
-                             completion-function
-                             executable
-                             save-runtime-options
-                             compression)
-  (declare (type function restart-function completion-function))
-  (let* (pid
-         (connection *emacs-connection*)
-         (style (connection.communication-style connection)))
-    (labels ((restart-sbcl ()
-               (sb-debug::enable-debugger)
-               (setf sb-impl::*descriptor-handlers* nil)
-               (setq *connections* nil *emacs-connection* nil)
-               (mm/reset-logging)
-               (funcall restart-function))
-             (logg (&rest args)
-               (ignore-errors 
-                (apply #'format sb-sys:*tty* args)
-                (terpri sb-sys:*tty*)
-                (finish-output sb-sys:*tty*)))
-             (waiter (&optional fd)
-               ;; (logg "FD-HANDLER for pid ~d fd ~d" pid fd)
-               (when fd 
-                 ;; (logg "before invalidate ~d" fd) 
-                 (sb-sys:invalidate-descriptor fd) 
-                 ;; (logg "before close ~d" fd)
-                 (sb-posix:close fd))
-               ;; (logg "before waitpid")
-               (multiple-value-bind (rpid status) (sb-posix:waitpid pid 0)
-                 ;; (logg "waitpid returned")
-                 (assert (= pid rpid))
-                 (assert (sb-posix:wifexited status))
-                 (funcall completion-function
-                          (zerop (sb-posix:wexitstatus status))))
-               ;; (logg "After waitpid")
-               )
-             (dump ()
-               (sb-debug::disable-debugger)
-               (setf sb-impl::*descriptor-handlers* nil)
-               (setq *connections* nil *emacs-connection* nil)
-               (log4cl:clear-logging-configuration)
-               (apply 'log:config *mm/log-config*)
-               (sb-ext:save-lisp-and-die
-                filename
-                :executable executable 
-                :save-runtime-options save-runtime-options
-                :toplevel #'restart-sbcl
-                :save-runtime-options save-runtime-options
-                :compression compression))
-             (forker () 
-               (logg "I'm a forker")
-               (if (eq style :spawn) 
-                   (cond ((= (setq pid (sb-posix:fork)) 0)
-                          ;; (logg "I'm a child in forker()")
-                          (dump))
-                         (t
-                          ;; (logg "I'm a parent of ~d, will use thread to wait" pid)
-                          (spawn #'waiter :name "background-save-image")))
-                   (multiple-value-bind (pipe-in pipe-out) (sb-posix:pipe)
-                     (cond ((= (setq pid (sb-posix:fork)) 0)
-                            (sb-posix:close pipe-in)
-                            (dump))
-                           (t
-                            ;; (logg "I'm a parent of ~d, will use fd-handler to wait" pid)
-                            (sb-posix:close pipe-out)
-                            (sb-sys:add-fd-handler pipe-in :input #'waiter)
-                            ;; (logg "Added fd handler to ~d~% handlers are now ~s" pipe-in
-                            ;;       sb-impl::*descriptor-handlers*)
-                            ))))))
-      (call-with-only-initial-thread #'forker))))
-
-(defun %mydump-completed (success)
-  (log:info "Dumping lisp image ~A ~:[failed!~;succeeded.~]" "/home/max/test1" success))
-
-(defmacro clon:dump (name function &rest args) 
-  `(progn
-     (setq clon::*executablep* t) ; not used but here for correctness
-     (mydump ,name
-             :restart-function #',function
-             :executable t
-             :save-runtime-options t
-             :completion-function
-             (lambda (success) 
-               (log:info "Dumping lisp image ~A ~:[failed!~;succeeded.~]" ,name success))
-             ,@args)))
 
 (log:info "~~/.swank.lisp done")
